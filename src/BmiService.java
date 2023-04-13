84af8e7352ae424afbf95b7c46af2f3e33de0c07
Rinat <rinat-yar@mail.ru>
2023-04-13 19:30:30 +0300

    Initial commit_2

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
     public double calculate(double weight, double height) {
 
-        return weight / (height * height);
+        return (int) (weight / height / height);
     }
 }
